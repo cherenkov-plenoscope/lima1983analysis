e936a37175037479db5d86cf07dc75668adf8d60
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-05-18 16:13:54 +0200

    bumb, prepare pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import setuptools
-import os
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="lima1983analysis_sebastian-achim-mueller",
-    version="0.0.0",
+    version="0.0.1",
     description="Sensitivity, Ti-Pei Li and Yu-Qian-Ma, APJ 272:317-324, 1983",
     long_description=long_description,
-    url="https://github.com/cherenkov-plenoscope",
+    long_description_content_type='text/markdown',
+    url="https://github.com/cherenkov-plenoscope/lima1983analysis",
     author="Sebastian Achim Mueller",
     author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
-    license="MIT",
     packages=["lima1983analysis",],
     classifiers=[
         "Programming Language :: Python :: 3",
